3d533778f1ceb5cdf9f10465433f27ed59c811c4
gerardojgarcia <gerardojgarcia@pm.me>
2022-12-26 20:29:21 -0800

    added quote container

--- pages/quote.vue
+++ pages/quote.vue
@@ -6,6 +6,31 @@ export default {
 
 <template>
 
+
+
+  <section class="">
+    <div class="form-container pt-32 grid place-items-center pb-32">
+    
+    <form action="submit" class="flex flex-col border-0 border-black p-10 w-1/2 space-y-3 rounded-xl text-left justify-center shadow-lg">
+
+    <strong class="text-black text-5xl">Get A Quote</strong>
+
+    <label for="name">Full Name</label>
+      <input type="text" class="bg-gray-300 rounded-xl p-2  w-1/3" required>
+      <label for="email">Email</label>
+      <input type="email" class="bg-gray-300 rounded-xl p-2  w-1/3" required>
+      <label for="address">Address</label>
+      <input type="text" name="address" class="bg-gray-300 rounded-xl p-2  w-1/3" required>
+
+
+
+    </form>
+    
+    
+    </div>
+  </section>
+
+
 </template>
 
 <style scoped>
