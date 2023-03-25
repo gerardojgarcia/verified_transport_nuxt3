92b94d2bbb0430d68c2444c945052773afd45ba8
Gerardo J Garcia <gerardojgarcia@pm.me>
2023-03-24 18:53:24 -0700

    responsive layout changes

--- pages/about.vue
+++ pages/about.vue
@@ -5,13 +5,83 @@ export default {
 </script>
 
 <template>
-<section>
+<section >
 
-  <h1 class="text-6xl font-bold">About</h1>
+
+
+
+
+
+
+  <div class="pt-32 flex flex-col justify-center" >
+
+    <h1 class="text-6xl text-center font-bold">About</h1>
+
+
+    <div class="image-container rounded-lg m-auto my-16">
+
+
+      <img src="/imgs/scott_rever.png" alt="image of scott harwood"/>
+
+    </div>
+
+
+    <div class="about-text m-auto flex flex-col items-center space-y-16 pb-16">
+
+
+      <p class="px-8 w-1/3 ">  Scott Harwood is a successful entrepreneur and the owner of a thriving logistics company that specializes in same-day, last-minute shipping services. With over 25 years of experience in the industry, Scott has built a reputation for his exceptional leadership skills, attention to detail, and dedication to providing top-notch customer service.</p>
+
+
+      <p class="px-8 w-1/3">Born and raised in Lake Elsinore, Scott has always been passionate about logistics and transportation. He started his career in the industry as a delivery driver, working his way up through the ranks and gaining valuable experience in all aspects of the business.</p>
+
+
+      <p class="px-8 w-1/3">In 2005, Scott founded his own logistics company, with the aim of providing fast, reliable, and affordable shipping services to businesses of all sizes. Over the years, his company has grown into a mid-size enterprise with a loyal customer base and a team of highly skilled and dedicated employees.</p>
+
+
+      <p class="px-8 w-1/3">As the owner of the company, Scott is committed to maintaining the highest standards of professionalism and excellence in all aspects of the business. He works closely with his team to ensure that every customer receives the best possible service, and he is always looking for ways to improve and innovate in order to stay ahead of the competition.</p>
+
+      <p class="px-8 w-1/3">Despite his busy schedule, Scott remains actively involved in the Lake Elsinore community and is a passionate supporter of local charities and non-profit organizations. He is a true leader and role model, and his dedication and hard work have earned him the respect and admiration of his peers and colleagues in the logistics industry.</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+  </div>
 
 </section>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+
+
+.image-container {
+  width: 25%;
+  object-fit: cover;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+  }
+}
 
 </style>
